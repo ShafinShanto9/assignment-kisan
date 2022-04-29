49be4bea44804514c57ed80cc2280eecca6308f8
shafin_shant0 <shafinshanto9@gmail.com>
2022-04-30 01:24:40 +0600

    working on main section

--- src/components/Main/RightSide.js
+++ src/components/Main/RightSide.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const RightSide = () => {
   return (
-    <div>RightSide</div>
+    <div>
+        <h2 style={{ fontWeight: "bold" }}>Justo Vulputate Tortor Sem</h2>
+          <p style={{ fontSize: '25px' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quaerat officiis nam doloribus qui quos animi aliquid rerum unde quo beatae ipsa, quisquam obcaecati. Dolores dolorem odit ipsa quam aliquid?</p> 
+          
+          <p className='my-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate in laudantium exercitationem eos quod perferendis dignissimos atque maxime sunt hic incidunt beatae eum fugiat expedita laborum voluptates, doloremque nesciunt vitae delectus quas, reprehenderit magni ducimus consequuntur minima. Recusandae mollitia quibusdam, odit aspernatur impedit saepe, unde magni soluta fugit fuga veniam esse, facilis quisquam deleniti amet quo. Voluptatem eveniet tempore nemo doloremque nisi quod reprehenderit aliquam aspernatur explicabo maiores consectetur, inventore exercitationem velit? Nesciunt modi, perferendis minima delectus sequi reprehenderit iusto.</p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, autem. Ducimus numquam officiis architecto sit. Itaque incidunt quod ducimus quidem officiis vero voluptatum minima eum quia blanditiis reprehenderit velit, ad accusantium optio ex quae mollitia illo assumenda! Ab necessitatibus animi eligendi molestias ipsa deserunt doloremque <q style={{fontSize:"25px", padding:"15px",}}> debitis magnam tempora sapiente? Facilis dolores id alias laborum adipisci, qui quo cumque, hic nesciunt nostrum dolor in, aut possimus vitae mollitia beatae eum dolorum velit. Deserunt molestias ipsam, facere fuga quia nesciunt labore nemo eveniet rerum aut earum minima eos pariatur iure dicta vero non suscipit ab numquam illo nihil fugiat. Iste minus, nisi accusamus sed blanditiis eaque?</q> Obcaecati dolores in consectetur quisquam quidem consequuntur saepe soluta adipisci veritatis commodi. Veritatis quas, animi necessitatibus mollitia, inventore tempore, distinctio omnis ipsam non eius corporis sequi. Tempora aspernatur, odio repellendus rem mollitia unde corporis ab ullam, dolores voluptatem quod quas reprehenderit? Minima maiores obcaecati sunt ipsa.</p>
+          
+    </div>
   )
 }
 
